d86ace4b436e0f30b7958024b18cc66c2730f764
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2023-11-08 17:35:35 +0000

    naughty big commit - it actually works now though

--- workshop-creator.py
+++ workshop-creator.py
@@ -6,34 +6,39 @@ import time
 
 PYTHON_FOR_BEGINNERS = "Python For Beginners"
 GANS_WITH_PYTHON = "GANs with Python"
-INTRO_TO_GITHUB = "Introduction to GitHub and Version Control"
+INTRO_TO_GITHUB = "Intro to GitHub and Version Control"
 
 MIN_ATTENDANCE = 3
 MAX_ATTENDANCE = 0
-DAYS_BEFORE_WORKSHOP_OPENS = 7
+WEEKS_BEFORE_WORKSHOP_OPENS = 1
 
 DATE_IDX = 1
 START_TIME_IDX = 2
 END_TIME_IDX = 4
+DATE_STRING_LENGTH = len("DD/MM/YYYY")
 
-EVENT_INFO_Y = 518
-ADD_EVENT_POS = (2958, 296)
-EVENT_DATE_POS = (3364, EVENT_INFO_Y)
-DONE_BUTTON_POS = (4403, 914)
-START_TIME_POS = (3656, EVENT_INFO_Y)
-FINISH_TIME_POS = (4007, EVENT_INFO_Y)
-APPROX_TIME_POS = (4330, EVENT_INFO_Y)
-MIN_ATTEND_POS = (5009, EVENT_INFO_Y)
-MAX_ATTENT_POS = (5371, EVENT_INFO_Y)
-NOTES_POS = (2931, 739)
+EVENT_INFO_Y = 535
+ADD_EVENT_POS = (2988, 301)
+DONE_BUTTON_POS = (4409, 936)
+HEADING_POS = (2935, 416)
+DATE_INPUT_POS = (4432, 852)
+EVENT_DATE_POS = (3335, EVENT_INFO_Y)
+START_TIME_POS = (3672, EVENT_INFO_Y)
+FINISH_TIME_POS = (3956, EVENT_INFO_Y)
+APPROX_TIME_POS = (4331, EVENT_INFO_Y)
+MIN_ATTEND_POS = (5021, EVENT_INFO_Y)
+MAX_ATTENT_POS = (5365, EVENT_INFO_Y)
+NOTES_POS = (2987, 748)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "workshop", nargs="?", default=PYTHON_FOR_BEGINNERS
-)  # change when this is finished
+    "workshop"
+)
+parser.add_argument("cutoff", args="?", default=datetime.datetime.today().strftime('%d/%m/%Y'))
 args = parser.parse_args()
 
+CUT_OFF_DATE = datetime.datetime.strptime(args.cutoff, "%d/%m/%Y")
 
 def _get_ordinal_day(day: str) -> str:
     """Get the ordinal format of a day.
@@ -60,13 +65,14 @@ def _create_registration_message(date: str) -> str:
     """Creates a message about when a workshop will open.
 
     Args:
-        date (str): The date of the workshop in the form DD/MM/YYY.
+        date (str): The date of the workshop in the form DD/MM/YYYY.
 
     Returns:
         str: A string saying registration for the workshop will open a week before its start date.
     """
+    print(date)
     date = datetime.datetime.strptime(date, "%d/%m/%Y") - datetime.timedelta(
-        days=DAYS_BEFORE_WORKSHOP_OPENS
+        weeks=WEEKS_BEFORE_WORKSHOP_OPENS
     )
     day_name = date.strftime("%A")
     ordinal_day_of_month = _get_ordinal_day(date.strftime("%d"))
@@ -77,6 +83,7 @@ def add_event():
     """Clicks the Add Event button.
     """
     pyautogui.click(*ADD_EVENT_POS)
+    time.sleep(2)
 
 def set_event_info(pos: tuple, input: str):
     """Enters some information in the ORB workshop event fields.
@@ -89,21 +96,62 @@ def set_event_info(pos: tuple, input: str):
     time.sleep(1)
     pyautogui.write(input)
 
-print("15 seconds to make sure your mouse is in the right place...")
-time.sleep(15)
+def set_event_date(date: str):
+    """Enters the event date.
+
+    Args:
+        date (str): The date of the workshop in a DD/MM/YYY format.
+    """
+    pyautogui.click(*EVENT_DATE_POS)
+    pyautogui.click(*DATE_INPUT_POS, clicks=2, interval=1)
+    for _ in range(DATE_STRING_LENGTH):
+        pyautogui.press("backspace")
+    pyautogui.write(date)
+    time.sleep(1)
+    pyautogui.click(*DONE_BUTTON_POS)
+    time.sleep(2)
+    print(date)
+
+def before_cut_off_date(workshop_date: str) -> bool:
+    """Checks if the workshop date is before or on the cut off date.
+
+    Args:
+        workshop_date (str): The date of the workshop.
+
+    Returns:
+        bool: True if the workshop is on or before the cut off date, False otherwise.
+    """
+    return datetime.datetime.strptime(workshop_date, "%d/%m/%Y") <= CUT_OFF_DATE
+
+def set_event_times(position: tuple, time: str):
+    """Sets the time info for the workshop.
+
+    Args:
+        position (tuple): The position of the event start/end time field.
+        time (str): The time that should be inputted.
+    """
+    set_event_info(position, time)
+    pyautogui.press("enter")
+    pyautogui.press("down")
+    pyautogui.press("enter")
+
+DELAY = 5
+print(f"{str(DELAY)} seconds to make sure your mouse is in the right place...")
+time.sleep(DELAY)
 
 with open("workshops.csv", "r") as workshops_file:
     workshops = csv.reader(workshops_file, delimiter=",")
-    for row in workshops:
+    for row in reversed(list(workshops)):
         if len(row) > 0 and row[0].lower() == args.workshop.lower():
+            if before_cut_off_date(row[DATE_IDX]):
+                continue
             add_event()
-            set_event_info(EVENT_DATE_POS, row[DATE_IDX])
-            # Press the done button after giving date
-            pyautogui.click(*DONE_BUTTON_POS)
-            time.sleep(1)
-            set_event_info(START_TIME_POS, row[START_TIME_IDX])
-            set_event_info(FINISH_TIME_POS, row[END_TIME_IDX])
+            set_event_date(row[DATE_IDX])
+            set_event_times(START_TIME_POS, row[START_TIME_IDX][:-3])
+            set_event_times(FINISH_TIME_POS, row[END_TIME_IDX][:-3])
             set_event_info(MIN_ATTEND_POS, str(MIN_ATTENDANCE))
             set_event_info(MAX_ATTENT_POS, str(MAX_ATTENDANCE))
-            set_event_info(NOTES_POS, "test test " + _create_registration_message(row[DATE_IDX]))
+            set_event_info(HEADING_POS, row[0] + " in WG28B")
+            set_event_info(NOTES_POS, _create_registration_message(row[DATE_IDX]))
+            pyautogui.scroll(600)
 
